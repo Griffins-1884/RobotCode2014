1c6901bdc184594eb9c0fe7898dfbc3411dfbfb9
Colin Poler <colin_poler@asl.org>
2014-02-18 17:07:31 +0000

    General cleanup

--- Robot/src/org/usfirst/frc0/Robot/commands/XBoxDrive.java
+++ Robot/src/org/usfirst/frc0/Robot/commands/XBoxDrive.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.usfirst.frc0.Robot.commands;
+
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc0.Robot.Robot;
 
-/**
- *
- * @author jwang
- */
 public class XBoxDrive extends Command {
 
     public XBoxDrive() {
         requires(Robot.driveTrain);
     }
-    
+
     protected void initialize() {
         //do something here
     }
@@ -40,9 +31,9 @@ public class XBoxDrive extends Command {
         Robot.driveTrain.setLeftValues(0);
         Robot.driveTrain.setRightValues(0);
     }
-    
+
     protected void stop() {
-        
+
     }
-    
+
 }
