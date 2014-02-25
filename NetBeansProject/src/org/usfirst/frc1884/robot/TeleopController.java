6439c7fc3ce69e14a90b54603d8909fe1f58f90e
polerc <colin_poler@asl.org>
2014-02-25 16:40:07 +0000

    Oops, forgot to call teleopdrive...

--- NetBeansProject/src/org/usfirst/frc1884/robot/TeleopController.java
+++ NetBeansProject/src/org/usfirst/frc1884/robot/TeleopController.java
@@ -2,11 +2,12 @@ package org.usfirst.frc1884.robot;
 
 import org.usfirst.frc1884.robot.commands.LowerBlocker;
 import org.usfirst.frc1884.robot.commands.RaiseBlocker;
+import org.usfirst.frc1884.robot.commands.TeleopDrive;
 import org.usfirst.frc1884.robot.oi.OI;
 
 public class TeleopController {
     public static void init() {
-        
+        TeleopDrive.start();
     }
     public static void periodic() {
         if(OI.isBooleanChanged(OI.BLOCKER_EXTEND) && OI.isBooleanTrue(OI.BLOCKER_EXTEND)) {
@@ -19,5 +20,6 @@ public class TeleopController {
         // Execute every command!!!
         RaiseBlocker.execute();
         LowerBlocker.execute();
+        TeleopDrive.execute();
     }
 }
