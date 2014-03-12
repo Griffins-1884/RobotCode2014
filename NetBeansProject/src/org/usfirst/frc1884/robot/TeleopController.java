ea28f7673946c22c91b3577388493097bf932405
polerc <colin_poler@asl.org>
2014-03-12 18:16:17 +0000

    Added support for the feeder.

--- NetBeansProject/src/org/usfirst/frc1884/robot/TeleopController.java
+++ NetBeansProject/src/org/usfirst/frc1884/robot/TeleopController.java
@@ -1,17 +1,15 @@
 package org.usfirst.frc1884.robot;
 
-import org.usfirst.frc1884.robot.commands.DriveShiftHigh;
-import org.usfirst.frc1884.robot.commands.DriveShiftLow;
 import org.usfirst.frc1884.robot.commands.DriveShiftSwitch;
 import org.usfirst.frc1884.robot.commands.ExtendFeeder;
+import org.usfirst.frc1884.robot.commands.IntakeFeeder;
+import org.usfirst.frc1884.robot.commands.OuttakeFeeder;
 import org.usfirst.frc1884.robot.commands.ReloadShooter;
 import org.usfirst.frc1884.robot.commands.RetractFeeder;
 import org.usfirst.frc1884.robot.commands.ShootShooter;
 import org.usfirst.frc1884.robot.commands.TeleopDrive;
 import org.usfirst.frc1884.robot.oi.OI;
 import org.usfirst.frc1884.robot.subsystems.DriveTrain;
-import org.usfirst.frc1884.robot.subsystems.Intake;
-import org.usfirst.frc1884.robot.subsystems.Shooter;
 
 public class TeleopController {
     public static void init() {
@@ -31,6 +29,19 @@ public class TeleopController {
         }
         if(OI.whenPressed(OI.SHOOTER_FIRE)) {
             ShootShooter.instance.start();
+        }
+        
+        if(OI.whenPressed(OI.FEEDER_INTAKE)) {
+            IntakeFeeder.instance.start();
+        }
+        if(OI.whenPressed(OI.FEEDER_OUTTAKE)) {
+            OuttakeFeeder.instance.start();
+        }
+        if(OI.whenPressed(OI.FEEDER_EXTEND)) {
+            ExtendFeeder.instance.start();
+        }
+        if(OI.whenReleased(OI.FEEDER_EXTEND)) {
+            RetractFeeder.instance.start();
         }
     }
 }
